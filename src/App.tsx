32e92eea36e6291b123b3ceeb95dde84a3eacf6f
matiasperezperez-27 <matiasperezperez.27@gmail.com>
2025-08-12 14:50:41 +0200

    Error, probar 2

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/auth" element={<Auth />} />
+          <Route path="/login" element={<Auth />} />
           <Route path="/" element={<ProtectedRoute><Index /></ProtectedRoute>} />
           <Route path="/test-setup" element={<ProtectedRoute><TestSetup /></ProtectedRoute>} />
           <Route path="/quiz" element={<ProtectedRoute><Quiz /></ProtectedRoute>} />
